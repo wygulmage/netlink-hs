b49297a34d2664eead763e6922049004af669538
Markus Ongyerth <ongy@ongy.net>
2015-08-23 10:07:25 +0200

    Use WordXX types for values read from netlink

--- System/Linux/Netlink/GeNetlink/Control.hs
+++ System/Linux/Netlink/GeNetlink/Control.hs
@@ -1,15 +1,18 @@
 module System.Linux.Netlink.GeNetlink.Control
 (getFamilyId, CtrlAttribute(..), CtrlAttrMcastGroup(..), CtrlPacket(..),
-CTRLPacket, ctrlPacketFromGenl, CtrlAttrOpData(..))
+CTRLPacket, ctrlPacketFromGenl, CtrlAttrOpData(..), ctrlPackettoGenl,
+getFamilyWithMulticasts)
 where
 
-
+import Control.Applicative ((<$>))
 import Data.Serialize.Get
---import Data.Serialize.Put
+import Data.Serialize.Put
 import Data.Map (fromList, lookup, toList, Map)
 import Control.Monad (liftM, liftM2, join)
-import Data.ByteString (ByteString, append)
+import Data.ByteString (ByteString, append, empty)
 import Data.ByteString.Char8 (pack, unpack)
+import Data.Word (Word16, Word32)
+import Data.Maybe (fromMaybe)
 
 import Prelude hiding (lookup)
 
@@ -18,19 +21,19 @@ import System.Linux.Netlink.Internal
 import System.Linux.Netlink.GeNetlink.Constants
 
 
-data CtrlAttrMcastGroup = CAMG {grpName :: String, grpId :: Int } deriving (Eq, Show)
-data CtrlAttrOpData = CAO {opId :: Int, opFlags :: Int } deriving (Eq, Show)
+data CtrlAttrMcastGroup = CAMG {grpName :: String, grpId :: Word32 } deriving (Eq, Show)
+data CtrlAttrOpData = CAO {opId :: Word32, opFlags :: Word32 } deriving (Eq, Show)
 
 data CtrlAttribute =
-  CTRL_ATTR_UNSPEC ByteString |
-  CTRL_ATTR_FAMILY_ID Int |
-  CTRL_ATTR_FAMILY_NAME String |
-  CTRL_ATTR_VERSION Int |
-  CTRL_ATTR_HDRSIZE Int |
-  CTRL_ATTR_MAXATTR Int |
-  CTRL_ATTR_OPS [CtrlAttrOpData] |
+  CTRL_ATTR_UNSPEC       ByteString |
+  CTRL_ATTR_FAMILY_ID    Word16 |
+  CTRL_ATTR_FAMILY_NAME  String |
+  CTRL_ATTR_VERSION      Word32 |
+  CTRL_ATTR_HDRSIZE      Word32 |
+  CTRL_ATTR_MAXATTR      Word32 |
+  CTRL_ATTR_OPS          [CtrlAttrOpData] |
   CTRL_ATTR_MCAST_GROUPS [CtrlAttrMcastGroup] |
-  CTRL_ATTR_UNKNOWN Int ByteString
+  CTRL_ATTR_UNKNOWN      Int ByteString
   deriving (Eq, Show)
 
 
@@ -44,12 +47,17 @@ data CtrlPacket = CtrlPacket
 
 type CTRLPacket = GenlPacket NoData
 
+-- TODO fix error handling for nested arguments
+
 --
 -- Start ctrl utility
 --
 
-getInt :: ByteString -> Maybe Int
-getInt x = liftM fromIntegral $e2M $runGet getWord16host x
+getW16 :: ByteString -> Maybe Word16
+getW16 x = e2M (runGet getWord16host x)
+
+getW32 :: ByteString -> Maybe Word32
+getW32 x = e2M (runGet getWord32host x)
 
 e2M :: Either a b -> Maybe b
 e2M (Right x) = Just x
@@ -59,7 +67,7 @@ getMcastGroupAttr :: (Int, ByteString) -> Maybe CtrlAttrMcastGroup
 getMcastGroupAttr (_, x) = 
   let name = join $liftM (lookup (eCTRL_ATTR_MCAST_GRP_NAME :: Int)) attrs
       fid  = join $liftM (lookup (eCTRL_ATTR_MCAST_GRP_ID :: Int)) attrs in
-    liftM2 CAMG (liftM (init . unpack) name) (getInt =<< fid)
+    liftM2 CAMG (fmap (init . unpack) name) (getW32 =<< fid)
   where attrs = e2M $runGet getAttributes x
 
 getMcastGroupAttrs :: ByteString -> Maybe [CtrlAttrMcastGroup]
@@ -71,7 +79,7 @@ getOpAttr :: (Int, ByteString) -> Maybe CtrlAttrOpData
 getOpAttr (_, x) =
   let oid = join $liftM (lookup (eCTRL_ATTR_OP_ID :: Int)) attrs
       ofl  = join $liftM (lookup (eCTRL_ATTR_OP_FLAGS :: Int)) attrs in
-    liftM2 CAO (getInt =<< oid) (getInt =<< ofl)
+    liftM2 CAO (getW32 =<< oid) (getW32 =<< ofl)
   where attrs = e2M $runGet getAttributes x
 
 getOpAttrs :: ByteString -> Maybe [CtrlAttrOpData]
@@ -80,34 +88,54 @@ getOpAttrs x = case runGet getAttributes x of
   _ -> Nothing
 
 getAttribute :: (Int, ByteString) -> CtrlAttribute
-getAttribute (i, x) = let a = makeAttribute i x in
-    case a of
-      Just y  -> y
-      Nothing -> CTRL_ATTR_UNKNOWN i x
+getAttribute (i, x) = fromMaybe (CTRL_ATTR_UNKNOWN i x) $makeAttribute i x
 
 makeAttribute :: Int -> ByteString -> Maybe CtrlAttribute
 makeAttribute i x
   | i == eCTRL_ATTR_UNSPEC = Just $CTRL_ATTR_UNSPEC x
-  | i == eCTRL_ATTR_FAMILY_ID = liftM CTRL_ATTR_FAMILY_ID $getInt x
+  | i == eCTRL_ATTR_FAMILY_ID = fmap CTRL_ATTR_FAMILY_ID $getW16 x
   | i == eCTRL_ATTR_FAMILY_NAME = Just $CTRL_ATTR_FAMILY_NAME $unpack x
-  | i == eCTRL_ATTR_VERSION = liftM CTRL_ATTR_VERSION $getInt x
-  | i == eCTRL_ATTR_HDRSIZE = liftM CTRL_ATTR_HDRSIZE $getInt x
-  | i == eCTRL_ATTR_MAXATTR = liftM CTRL_ATTR_MAXATTR $getInt x
-  | i == eCTRL_ATTR_OPS = liftM CTRL_ATTR_OPS $getOpAttrs x
-  | i == eCTRL_ATTR_MCAST_GROUPS = liftM CTRL_ATTR_MCAST_GROUPS $getMcastGroupAttrs x
+  | i == eCTRL_ATTR_VERSION = fmap CTRL_ATTR_VERSION $getW32 x
+  | i == eCTRL_ATTR_HDRSIZE = fmap CTRL_ATTR_HDRSIZE $getW32 x
+  | i == eCTRL_ATTR_MAXATTR = fmap CTRL_ATTR_MAXATTR $getW32 x
+  | i == eCTRL_ATTR_OPS = fmap CTRL_ATTR_OPS $getOpAttrs x
+  | i == eCTRL_ATTR_MCAST_GROUPS = fmap CTRL_ATTR_MCAST_GROUPS $getMcastGroupAttrs x
   | otherwise = Nothing
 
-getAttributesFromList :: [(Int, ByteString)] -> [CtrlAttribute]
-getAttributesFromList xs = map getAttribute xs
 
 ctrlAttributesFromAttributes :: Map Int ByteString -> [CtrlAttribute]
-ctrlAttributesFromAttributes m = getAttributesFromList $toList m
-
+ctrlAttributesFromAttributes = map getAttribute . toList
 
 ctrlPacketFromGenl :: CTRLPacket -> CtrlPacket
 ctrlPacketFromGenl (GenericPacket h g attrs) = CtrlPacket h (genlDataHeader g) a
   where a = ctrlAttributesFromAttributes attrs
 ctrlPacketFromGenl _ = undefined
+
+putW16 :: Word16 -> ByteString
+putW16 x = runPut (putWord16host x)
+
+putW32 :: Word32 -> ByteString
+putW32 x = runPut (putWord32host x)
+
+--TODO maybe add those two, but generally we shouldn't send these anyway
+cATA :: CtrlAttribute -> (Int, ByteString)
+cATA (CTRL_ATTR_UNSPEC       x) = (eCTRL_ATTR_UNSPEC      , x)
+cATA (CTRL_ATTR_FAMILY_ID    x) = (eCTRL_ATTR_FAMILY_ID   , putW16 x)
+cATA (CTRL_ATTR_FAMILY_NAME  x) = (eCTRL_ATTR_FAMILY_NAME , pack x)
+cATA (CTRL_ATTR_VERSION      x) = (eCTRL_ATTR_VERSION     , putW32 x)
+cATA (CTRL_ATTR_HDRSIZE      x) = (eCTRL_ATTR_HDRSIZE     , putW32 x)
+cATA (CTRL_ATTR_MAXATTR      x) = (eCTRL_ATTR_MAXATTR     , putW32 x)
+cATA (CTRL_ATTR_OPS          _) = (eCTRL_ATTR_OPS         , empty)
+cATA (CTRL_ATTR_MCAST_GROUPS _) = (eCTRL_ATTR_MCAST_GROUPS, empty)
+cATA (CTRL_ATTR_UNKNOWN    i x) = (i                      , x)
+
+ctrlAttributesToAttribute :: CtrlAttribute -> (Int, ByteString)
+ctrlAttributesToAttribute = cATA
+
+
+ctrlPackettoGenl :: CtrlPacket -> CTRLPacket
+ctrlPackettoGenl (CtrlPacket h g attrs)= GenericPacket h (GenlData g NoData) a
+  where a = fromList $map ctrlAttributesToAttribute attrs
 
 
 
@@ -118,20 +146,18 @@ familyIdRequest name = let
   attrs = fromList [(eCTRL_ATTR_FAMILY_NAME, pack name `append` pack "\0")] in
     GenericPacket header (GenlData geheader NoData) attrs
 
-getFromPacket :: (GenlPacket b -> a) -> (GenlPacket b) -> a
-getFromPacket _ (GenericError _  code _) = error (show code)
-getFromPacket f x = f x
-
-getRight :: (Either String a) -> a
-getRight (Right x) = x
-getRight (Left x)  = error x
-
 getFamilyId :: NetlinkSocket -> String -> IO Int
-getFamilyId sock name = do
-  packet <- queryOneN sock (familyIdRequest name)
-  let fid = getFromPacket get packet
-  return (getId fid)
-  where getId (Just x) = fromIntegral $getRight (runGet getWord16host x)
-        getId Nothing  = -1
-        get = \(GenericPacket _ _ a) -> lookup eCTRL_ATTR_FAMILY_ID a
+getFamilyId s m = fmap fst (getFamilyWithMulticasts s m)
 
+
+getFamilyWithMulticasts :: NetlinkSocket -> String -> IO (Int, [CtrlAttrMcastGroup])
+getFamilyWithMulticasts sock name = do
+  (CtrlPacket _ _ attrs) <- ctrlPacketFromGenl <$> queryOneN sock (familyIdRequest name)
+  return (fromIntegral $getIdFromList attrs, getMCFromList attrs)
+  where getIdFromList (CTRL_ATTR_FAMILY_ID x:_) = x
+        getIdFromList (_:xs) = getIdFromList xs
+        getIdFromList [] = -1
+        
+        getMCFromList (CTRL_ATTR_MCAST_GROUPS x:_) = x
+        getMCFromList (_:xs) = getMCFromList xs
+        getMCFromList [] = []
